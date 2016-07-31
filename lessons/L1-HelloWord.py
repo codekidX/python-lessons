08b43fcd76fb2a37869dd6a378d80a5513d209af
codekidX <ckrackerx15@gmail.com>
2016-07-31 18:24:02 +0530

    lesson 1

---
+++ lessons/L1-HelloWord.py
@@ -0,0 +1,10 @@
+# make sure you have installed python
+# visit https://www.python.org/downloads/
+
+# in python there ways to write hello world (BTW you comment by starting with '#', incase you didn't notice)
+# MOVING ON
+
+# now run this code : Type python [ENTER]
+
+print 'Hello World' #notice no semi colon since python understands next line is start of new code
+print "Welcome to Batman's World" # double quotation is useful if u have a single quote in your string
