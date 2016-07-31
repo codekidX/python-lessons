39a74b0eb4040b298f296d8c85c869fbe63ab5a0
codekidX <ckrackerx15@gmail.com>
2016-07-31 19:00:17 +0530

    add lesson 2

---
+++ lessons/L2.py
@@ -0,0 +1,26 @@
+# Lesson 2 - types of variables
+# DO python L2.py and compare output and code
+
+# use buit-in type function to know variable types
+
+# 'hello' is stored in x (NOTICE: no type definition required) like String x or int x
+x = 'hello'
+print "x is 'hello'"
+print type(x) # shows which type of variable x is
+print ' ' #blank
+print ' '
+x = 5
+print "x is 5"
+print type(x) # shows which type of variable x is
+print ' ' #blank
+print ' '
+x = 3.3
+print "x is 3.3"
+print type(x) # shows which type of variable x is
+print ' ' #blank
+print ' '
+x = 555555555555555555555
+print "x is 555555555555555555555"
+print type(x) # shows which type of variable x is
+print ' ' #blank
+print ' '
