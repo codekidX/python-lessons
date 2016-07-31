8b79945c5d6efb9cf3c496671da51390250561d2
codekidX <ckrackerx15@gmail.com>
2016-07-31 19:10:04 +0530

    add lesson 3

---
+++ lessons/L3.py
@@ -0,0 +1,31 @@
+# Lesson 3 - Conversion between types
+# Do python L3.py and compare output with code
+
+print "x is integer with value 5"
+x = 5
+# Lesson 3 - Conversion between types
+# Do python L3.py and compare output with code
+
+print "now do str(x), This converts int to string"
+x = str(x)
+print ' ' #blank
+print ' '
+print "Now lets see which type x is" 
+print type(x)
+print ' ' #blank
+print ' '
+print "now do int(x), This converts string back to int"
+x = int(x)
+print ' ' #blank
+print ' '
+print "Now lets see which type x is" 
+print type(x)
+print ' ' #blank
+print ' '
+print "now do float(x), This converts int to float"
+x = float(x)
+print ' ' #blank
+print ' '
+print "Now lets see which type x is" 
+print type(x)
+
